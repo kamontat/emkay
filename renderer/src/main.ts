8f348886d733f1c47fafe464e7107da9bb279a4f
Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>
2024-12-09 18:50:18 +0700

    feat: add speed insight and analytics from vercel
    Signed-off-by: Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>

--- renderer/src/main.ts
+++ renderer/src/main.ts
@@ -1,9 +1,14 @@
+import { injectSpeedInsights } from '@vercel/speed-insights';
+import { inject as injectAnalytics } from '@vercel/analytics'
+
 import "./styles/app.css";
 import "./styles/mymk.css";
 import "./styles/reset.css";
 
 import App from "./App.svelte";
 
+injectSpeedInsights();
+injectAnalytics();
 const app = new App({
   target: document.getElementById("app"),
 });
